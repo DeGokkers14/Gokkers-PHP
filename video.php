2492e4b8f85e0c485285730f4704013b5a405ea8
emiiil <36730035+emiiil@users.noreply.github.com>
2018-03-12 14:24:26 +0100

    Add files via upload

---
+++ video.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>De Gokkers</title>
+</head>
+<body>
+    <form action="test.php" method="post">
+        <label>Email:</label>
+        <input type="email" name="email">
+        <label>Wachtwoord:</label>
+        <input type="password" minlength="7" name="password">
+        <input type="submit">
+    </form>
+    <video width="320" height="240" controls>
+        <source src="videoplayback.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</body>
+</html>
+
+<?php
